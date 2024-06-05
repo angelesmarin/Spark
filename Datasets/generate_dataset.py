20f66f4f64cd6958d53f99d767147b7f1adcf6ae
Angeles Marin <angeles.marinbatana@bsu.edu>
2024-06-05 11:29:49 -0500

    Update generate_dataset.py

--- Datasets/generate_dataset.py
+++ Datasets/generate_dataset.py
@@ -1,19 +1,32 @@
 from faker import Faker
 import random
 import csv
-import os
 
 fake = Faker()
 
 def generate_dataset(num_points):
     dataset = []
     for _ in range(num_points):
-        feature1 = random.uniform(0, 100)
-        feature2 = random.uniform(0, 100)
-        feature3 = random.uniform(0, 100)
-        feature4 = random.uniform(0, 100)
-        feature5 = random.uniform(0, 100)
-        label = random.choice(['A', 'B'])
+        label = random.choice(['A', 'B', 'C'])
+        if label == 'A':
+            feature1 = random.uniform(10, 40)
+            feature2 = random.uniform(10, 40)
+            feature3 = random.uniform(10, 40)
+            feature4 = random.uniform(10, 40)
+            feature5 = random.uniform(10, 40)
+        elif label == 'B':
+            feature1 = random.uniform(60, 90)
+            feature2 = random.uniform(60, 90)
+            feature3 = random.uniform(60, 90)
+            feature4 = random.uniform(60, 90)
+            feature5 = random.uniform(60, 90)
+        else:  # label == 'C'
+            feature1 = random.uniform(120, 150)
+            feature2 = random.uniform(120, 150)
+            feature3 = random.uniform(120, 150)
+            feature4 = random.uniform(120, 150)
+            feature5 = random.uniform(120, 150)
+        
         dataset.append((feature1, feature2, feature3, feature4, feature5, label))
     return dataset
 
@@ -27,6 +40,6 @@ def save_dataset_to_csv(dataset, filename):
 if __name__ == "__main__":
     num_points = 1000  # You can adjust the number of points as needed
     dataset = generate_dataset(num_points)
-    desktop_path = '/Users/angelesmarin/Desktop/2Class_5Feature_dataset.csv'
+    desktop_path = '/Users/angelesmarin/Desktop/2C_5D_dataset.csv'
     save_dataset_to_csv(dataset, desktop_path)
     print(f"Dataset with {num_points} points generated and saved to '{desktop_path}'")
